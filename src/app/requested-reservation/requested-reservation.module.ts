5f77f3dbf0769509b98e2300ab04d44580dd79f6
Mo2024 <mohdosama2025@gmail.com>
2023-12-05 19:28:37 +0300

    added reject and approve buttons frontend

--- src/app/requested-reservation/requested-reservation.module.ts
+++ src/app/requested-reservation/requested-reservation.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,8 @@ import { RequestedReservationPage } from './requested-reservation.page';
     IonicModule,
     RequestedReservationPageRoutingModule
   ],
-  declarations: [RequestedReservationPage]
+  declarations: [RequestedReservationPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
-export class RequestedReservationPageModule {}
+export class RequestedReservationPageModule { }
